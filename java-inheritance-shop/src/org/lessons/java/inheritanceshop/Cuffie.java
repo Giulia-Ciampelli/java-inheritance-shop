84661d7cfc702b8103b1f49951c35f3f517e0341
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-02-21 17:35:42 +0100

    initial commit

---
+++ java-inheritance-shop/src/org/lessons/java/inheritanceshop/Cuffie.java
@@ -0,0 +1,5 @@
+package org.lessons.java.inheritanceshop;
+
+public class Cuffie {
+
+}
---
+++ java-inheritance-shop/src/org/lessons/java/inheritanceshop/Main.java
@@ -0,0 +1,7 @@
+package org.lessons.java.inheritanceshop;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I'm alive");
+    }
+}
